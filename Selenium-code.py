794f7c6116c8cb54e4e6fd5f5760a2d9f0933a7b
vinicius <viniciuacarvalhossp@gmail.com>
2020-04-08 15:33:48 -0300

    both tools added

---
+++ Selenium-code.py
@@ -0,0 +1,165 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+import time
+import os
+import csv
+import io
+
+options = Options()
+options.add_argument('start-maximized')
+options.add_argument('disable-infobars')
+options.add_argument('--disable-extensions')
+options.add_argument("--incognito")
+options.add_argument("--no-sandbox")
+options.add_argument("--disable-dev-shm-usage")
+
+scopus_id = ''
+ordered_by =
+#1 - All articles
+#2 - Date(Newest)
+#3 - Date(Oldest)
+#4 - Cited by(Highest)
+#5 - Cited by(Lowest)
+min_year =  #0 to ignore
+max_year =  #0 to ignore
+size = #max 200, just for ordered_by != 1
+
+cdriver = webdriver.Chrome(options=options, executable_path=r"") #needs to specify the webdriver path
+
+cdriver.get('https://www.scopus.com/authid/detail.uri?authorId='+scopus_id)
+
+#To close a popup window in authors page
+try:
+            janela = WebDriverWait(cdriver,5).until(EC.presence_of_element_located((By.ID, '_pendo-close-guide_')))
+            cdriver.find_element_by_id('_pendo-close-guide_').click()
+except:
+            print('aaaa')
+finally:
+            articles = []
+            author = cdriver.find_element_by_class_name('wordBreakWord').get_attribute("innerText")
+
+#Creating the author folder
+if not os.path.exists(author):
+    os.makedirs(author)
+
+box = WebDriverWait(cdriver,3).until(EC.presence_of_element_located((By.ID,'resultDataRow0')))
+
+if ordered_by != 1:
+                cdriver.find_elements_by_class_name('ui-selectmenu-text')[0].click()
+                #selecting article's classification
+                if ordered_by ==3:
+                    cdriver.find_element_by_id('ui-id-2').click()
+                    time.sleep(2)
+
+                elif ordered_by == 4:
+                    cdriver.find_element_by_id('ui-id-3').click()
+                    time.sleep(2)
+
+                elif ordered_by == 5:
+                    cdriver.find_element_by_id('ui-id-4').click()
+                    time.sleep(2)
+
+                cdriver.find_elements_by_class_name('ui-selectmenu-text')[1].click()
+                cdriver.find_element_by_id('ui-id-12').click()
+                time.sleep(3)
+
+                pages = cdriver.find_elements_by_class_name('ddmDocTitle')
+                years = cdriver.find_elements_by_class_name('ddmPubYr')
+                links = []
+
+                if (max_year != 0 and min_year !=0):
+                    for i in range(0,len(pages)):
+                        if(int(years[i].get_attribute("innerText"))<=max_year and int(years[i].get_attribute("innerText")) >= min_year):
+                            links.append(pages[i].get_attribute("href"))
+                elif (max_year != 0 and min_year ==0):
+                        for i in range(0,len(pages)):
+                            if(int(years[i].get_attribute("innerText"))<=max_year):
+                                links.append(pages[i].get_attribute("href"))
+                elif (max_year == 0 and min_year !=0):
+                        for i in range(0,len(pages)):
+                            if(int(years[i].get_attribute("innerText"))>=min_year):
+                                links.append(pages[i].get_attribute("href"))
+                count = 0
+                for link in links:
+                    if (count==size):
+                        break
+                    cdriver.get(link)
+
+                    if(cdriver.find_element_by_id('fwciValue').text ):#Check if FWCI value exist
+                        item = []
+                        count+=1
+                        #Articles name
+                        item.append(cdriver.find_element_by_xpath('//*[contains(concat( " ", @class, " " ), concat( " ", "h3", " " ))]').text)
+                        #FWCI
+                        item.append(cdriver.find_element_by_id('fwciValue').text)
+                        #Journal with date
+                        item.append(cdriver.find_element_by_id('journalInfo').text)
+                        #authors and co-authors
+                        anchors = cdriver.find_elements_by_xpath('//*[(@id = "authorlist")]//*[contains(concat( " ", @class, " " ), concat( " ", "anchorText", " " ))]')
+                        #authors count
+                        item.append(len(anchors))
+                        #prominence percentile
+                        item.append(cdriver.find_element_by_class_name('percentText').text)
+                        key_words = cdriver.find_elements_by_xpath('//*[(@id = "topicSection")]//*[contains(concat( " ", @class, " " ), concat( " ", "secondaryLink", " " ))]')
+                        #3 keywords by scopus
+                        item.append(key_words[1].get_attribute('innerText'))
+                        anchors_text = ''
+                        for i in anchors:
+                            anchors_text=anchors_text+"|"+i.text
+                        #name of authors and co-authors
+                        item.append(anchors_text)
+                        articles.append(item)
+                print(articles)
+
+
+else: #To harvest data of all articles
+    cdriver.get(cdriver.find_element_by_class_name('ddmDocTitle').get_attribute("href"))
+    next_button_wait = WebDriverWait(cdriver,5).until(EC.presence_of_element_located((By.CLASS_NAME,'nextLink')))
+    count = 0
+    while True:
+        try:
+            next_button_wait = WebDriverWait(cdriver,5).until(EC.presence_of_element_located((By.CLASS_NAME,'nextLink'))) #Verifica se há mais artigos para coletar
+            if (cdriver.find_element_by_id('fwciValue').text): #Verifica se existe o valor FWCI
+                    item = []
+                    count +=1
+                    #Articles name
+                    item.append(cdriver.find_element_by_class_name('h3').text)
+                    #FWCI
+                    item.append(cdriver.find_element_by_id('fwciValue').text)
+                    #Journal with date
+                    item.append(cdriver.find_element_by_id('journalInfo').text)
+                    #authors and co-authors
+                    anchors = cdriver.find_elements_by_xpath('//*[(@id = "authorlist")]//*[contains(concat( " ", @class, " " ), concat( " ", "anchorText", " " ))]')
+                    #authors count
+                    item.append(len(anchors))
+                    #prominence percentile
+                    item.append(cdriver.find_element_by_class_name('percentText').text)
+                    #3 keywords by scopus
+                    key_words = cdriver.find_elements_by_xpath('//*[(@id = "topicSection")]//*[contains(concat( " ", @class, " " ), concat( " ", "secondaryLink", " " ))]')
+                    item.append(key_words[1].get_attribute('innerText'))
+                    for i in anchors:
+                            anchors_text=anchors_text+"|"+i.text
+                    #name of authors and co-authors
+                    item.append(anchors_text)
+                    anchors_text = ''
+                    articles.append(item)
+            cdriver.find_element_by_class_name('nextLink').click()
+        except: #in case of next button does not appear
+            cdriver.refresh()
+            try:
+                next_button_wait = WebDriverWait(cdriver,5).until(EC.presence_of_element_located((By.CLASS_NAME,'nextLink'))) #Verifica se há mais artigos para coletar
+            except:
+                cdriver.refresh()
+                try:
+                    next_button_wait = WebDriverWait(cdriver,5).until(EC.presence_of_element_located((By.CLASS_NAME,'nextLink'))) #Verifica se há mais artigos para coletar
+                except:
+                    print(articles)
+                    break
+
+#Writing in csv
+c = csv.writer(io.open(author+"\\" + scopus_id +"_"+ str(ordered_by) +"_"+ str(size) +"_"+ str(min_year) +"_"+ str(max_year)+".csv", "w", encoding="utf-8",newline=''))
+c.writerow(["Article_name", "FWCI", "year" ,"authors_count","Prominence percentile","Topics","Anchors"])
+c.writerows(articles)
---
+++ main.py
@@ -0,0 +1,100 @@
+import pandas as pd
+import statistics
+import numpy
+import matplotlib.pyplot as plt
+from scipy import stats as s
+import os
+
+plt.rcParams['figure.figsize'] = (11,7)
+plt.rcParams['xtick.labelsize'] = 16
+plt.rcParams['ytick.labelsize'] = 16
+plt.rcParams['axes.labelsize'] = 18
+plt.rcParams['axes.titlesize'] = 16
+
+count_interval =
+
+def interval(df,attr):
+    fwcis = df[attr].values.copy()
+    fwcis.sort()
+    count = 0
+    dist = {}
+    #Counting articles in the specified interval
+    for i in numpy.arange(count_interval, max(fwcis)+count_interval,count_interval):
+        max_range = round(float(i)-count_interval,1)
+        dist[max_range] = 0
+        while count < len(fwcis) and fwcis[count] <= i:
+            dist[max_range] +=1
+            count+=1
+    return dist
+
+def plot(df, title, x, y,attr):
+    dist = interval(df,attr)
+    articles_count = dist.keys()
+    values = dist.values()
+    #ploting and saving the bar graphic
+    plt.bar(articles_count,values,width=count_interval,align='edge')
+    plt.title(title)
+    plt.ylabel(x)
+    plt.xlabel(y)
+    if not os.path.exists(file):
+                os.makedirs(file)
+    plt.savefig(file + "/" + title + ".png", format='png')
+    plt.show()
+
+#Plot 2 datasets in a single graphic
+def plot2(df1, df2, title, legend1, legend2, x, y, attr):
+    dist1 = interval(df1,attr)
+    dist2 = interval(df2,attr)
+    count1 = dist1.keys()
+    val1 = dist1.values()
+    count2 = dist2.keys()
+    val2 = dist2.values()
+    plt.bar(count1,val1, label = legend1,width=count_interval, color = 'gray',align='edge')
+    plt.title(title)
+    plt.ylabel(x)
+    plt.xlabel(y)
+    plt.bar(count2,val2,zorder=2, label = legend2,width=count_interval, alpha = 0.6, color = 'red',align='edge')
+    plt.legend()
+    if not os.path.exists(title):
+                os.makedirs(title)
+    plt.savefig(title + ".png", format='png')
+    plt.show()
+
+def divide_coauthors(df,attr):
+    divided_df = df.copy()
+    for i in range(0,len(df)):
+        divided_df.at[i,attr] = round(float(df.at[i,attr])/float(df.at[i,'authors_count']),2)
+    return divided_df
+
+def statistics_coauthors(df):
+    print("Co-author mean per article: " + str(round(statistics.mean(df['authors_count']),2)))
+    print("Co-author mode per article: " + str(s.mode(df['authors_count'])))
+    print("Co-author median per article: " + str(statistics.median(df['authors_count'])))
+
+def attribute_statistics(df, attr):
+    print(attr + " sum:"+ str(sum(df[attr])))
+    print(attr + " mean: " + str(round(statistics.mean(df[attr]),2)))
+    print(attr + " mode: " + str(s.mode(df[attr])))
+    print(attr + " standard deviation:" + str(round(statistics.pstdev(df[attr]),2)))
+    print(attr + " median:" + str(round(statistics.median(df[attr]),2)))
+
+def twenty_highest_att(df,attr):
+    df.sort_values(attr, inplace = True, ascending=False)
+    df.reset_index(drop=True, inplace=True)
+    df = df.head(20)
+    return df
+
+def scatter_coauthorsxfwci(df):
+    plt.scatter(df['authors_count'], df['FWCI'],  c= 'green', alpha=0.5)
+    plt.ylabel("FWCI")
+    plt.xlabel('Number of authors')
+    if not os.path.exists(file):
+        os.makedirs(file)
+    plt.savefig(file+'/scatter co-authors X FWCI.png', format='png')
+    plt.show()
+
+file = ''
+file2 = ''
+df = pd.read_csv(file+'.csv')
+if file2 != '':
+    df2 = pd.read_csv(file2+'.csv')
