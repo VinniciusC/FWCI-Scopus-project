aea55ebe05fad317543ff69570e08ed28923196d
Vinicius Carvalho <32166287+VinniciusC@users.noreply.github.com>
2020-05-12 16:40:51 -0300

    Improvements to handle with scopus errors

--- Selenium-code.py
+++ Selenium-code.py
@@ -16,27 +16,25 @@ options.add_argument("--incognito")
 options.add_argument("--no-sandbox")
 options.add_argument("--disable-dev-shm-usage")
 
-scopus_id = ''
-sorted_by =
+scopus_id = '7004438181'
+sorted_by = 1
 #1 - All articles
 #2 - Date(Newest)
 #3 - Date(Oldest)
 #4 - Cited by(Highest)
 #5 - Cited by(Lowest)
-min_year =  #0 to ignore
-max_year =  #0 to ignore
-size =  #max 200, for sorted_by == 1 or to get the maximum, fill with 0
+min_year = 0 #0 to ignore
+max_year = 0 #0 to ignore
+size = 0  #max 200, for sorted_by == 1 or to get the maximum, fill with 0
 
 cdriver = webdriver.Chrome(options=options, executable_path=r"chromedriver") #needs to specify the webdriver path
-	cdriver.implicitly_wait(10)
+cdriver.implicitly_wait(10)
 cdriver.get('https://www.scopus.com/authid/detail.uri?authorId='+scopus_id)
 
 #To close a popup window in authors page
 try:
             janela = WebDriverWait(cdriver,5).until(EC.presence_of_element_located((By.ID, '_pendo-close-guide_')))
             cdriver.find_element_by_id('_pendo-close-guide_').click()
-except:
-            print('Please run again')
 finally:
             articles = []
             author = cdriver.find_element_by_class_name('wordBreakWord').get_attribute("innerText")
@@ -45,7 +43,7 @@ finally:
 if not os.path.exists(author):
     os.makedirs(author)
 
-box = WebDriverWait(cdriver,3).until(EC.presence_of_element_located((By.ID,'resultDataRow0')))
+box = WebDriverWait(cdriver,30).until(EC.presence_of_element_located((By.ID,'resultDataRow0')))
 
 if sorted_by != 1:
                 cdriver.find_elements_by_class_name('ui-selectmenu-text')[0].click()
@@ -84,7 +82,7 @@ if sorted_by != 1:
                                 links.append(pages[i].get_attribute("href"))
                 count = 0
                 for link in links:
-                    if (count==size and size != 0)::
+                    if (count==size and size != 0):
                         break
                     cdriver.get(link)
 
@@ -93,6 +91,8 @@ if sorted_by != 1:
                         count+=1
                         #Articles name
                         item.append(cdriver.find_element_by_xpath('//*[contains(concat( " ", @class, " " ), concat( " ", "h3", " " ))]').text)
+                        if(cdriver.find_element_by_id('fwciValue').text == ''):
+                            continue
                         #FWCI
                         item.append(cdriver.find_element_by_id('fwciValue').text)
                         #Journal with date
@@ -105,7 +105,8 @@ if sorted_by != 1:
                         item.append(cdriver.find_element_by_class_name('percentText').text)
                         key_words = cdriver.find_elements_by_xpath('//*[(@id = "topicSection")]//*[contains(concat( " ", @class, " " ), concat( " ", "secondaryLink", " " ))]')
                         #3 keywords by scopus
-                        item.append(key_words[1].get_attribute('innerText'))
+                        if(key_words):
+                            item.append(key_words[1].get_attribute('innerText')) 
                         anchors_text = ''
                         for i in anchors:
                             anchors_text=anchors_text+"|"+i.text
@@ -127,6 +128,8 @@ else: #To harvest data of all articles
                     count +=1
                     #Articles name
                     item.append(cdriver.find_element_by_class_name('h3').text)
+                    if(cdriver.find_element_by_id('fwciValue').text == ''):
+                        continue
                     #FWCI
                     item.append(cdriver.find_element_by_id('fwciValue').text)
                     #Journal with date
@@ -148,20 +151,28 @@ else: #To harvest data of all articles
                     articles.append(item)
             cdriver.find_element_by_class_name('nextLink').click()
         except: #in case of next button does not appear
-            cdriver.refresh()
+            while(cdriver.current_url=='https://www.scopus.com/error.uri'): #Handling with scopus error
+                cdriver.execute_script("window.history.go(-1)")
+                try:
+                    WebDriverWait(cdriver,10).until(EC.presence_of_element_located((By.CLASS_NAME,'nextLink')))	
+                except:
+                    continue
             try:
                 next_button_wait = WebDriverWait(cdriver,5).until(EC.presence_of_element_located((By.CLASS_NAME,'nextLink'))) #Verifica se há mais artigos para coletar
+                cdriver.find_element_by_class_name('nextLink').click()
             except:
                 cdriver.refresh()
                 try:
                     next_button_wait = WebDriverWait(cdriver,5).until(EC.presence_of_element_located((By.CLASS_NAME,'nextLink'))) #Verifica se há mais artigos para coletar
+                    cdriver.find_element_by_class_name('nextLink').click()
                 except:
                     print(articles)
                     break
 
 #Writing in csv
-c = csv.writer(io.open(author+"\\" + scopus_id +"_"+ str(sorted_by) +"_"+ str(size) +"_"+ str(min_year) +"_"+ str(max_year)+".csv", "w", encoding="utf-8",newline=''))
-c.writerow(["Article_name", "FWCI", "year" ,"authors_count","Prominence percentile","Topics","Anchors"])
-c.writerows(articles)
-
-print("CSV file written with success")
+with open(author+"\\" + scopus_id +"_"+ str(sorted_by) +"_"+ str(size) +"_"+ str(min_year) +"_"+ str(max_year)+".csv", "w", encoding="utf-8",newline='') as f:
+    c = csv.writer(f)
+    c.writerow(["Article_name", "FWCI", "year" ,"authors_count","Prominence percentile","Topics","Anchors"])
+    c.writerows(articles)
+    f.close()
+    print("CSV file written with success")
